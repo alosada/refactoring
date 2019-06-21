369ae5dcadd13bc5a208c2d1360e387a6c053d5e
Alejandro E. Losada <alosada@gmail.com>
2019-06-21 14:06:10 -0500

    changed order of method for better readability, REFACTORED!

--- conekta_controller.rb
+++ conekta_controller.rb
@@ -11,42 +11,97 @@ class ConektaController < ApplicationController
 
   private
 
-  def order_post_process(order)
-    return paid_order_post_process if order.payment_status == "paid"
-    failed_order_post_process
+  def render_json_error
+    result[:status] ||= 'error'
+    result[:message] =  @error || 'Something went wrong :('
+    render json: result.to_json
   end
 
-  def paid_order_post_process
-    backer.update_attributes(back_success_attributes)
-    backer.confirm!
-    send_successful_back_emails(backer)
-    result[:status] = "approved"
-    result[:redirect] = success_conektum_path(backer)
+  def preparations_failed?
+    project.nil? ||
+    backer.nil? ||
+    backer.errors.any? ||
+    customer.nil? ||
+    order.nil?
   end
 
-  def back_success_attributes
-    charge = order.charges.first
+  def project
+    @project ||= Project.friendly.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    @error = 'Project missing.'
+    nil
+  end
+
+  def backer
+    @backer ||= create_backer
+  rescue StandardError
+    @error = backer.errors.full_messages.join(', ') if backer.errors.any?
+    @error ||= 'Backer missing.'
+    nil
+  end
+
+  def create_backer
+    Backer.create(backer_params) do |backer|
+      verify_recaptcha(model: backer, message: "Failed recaptcha.") if verify_recaptcha?
+      backer.reward = reward if reward.present? && backer.value >= reward.minimum_value 
+    end
+  end
+
+  def backer_params
     {
-      transaction_id: order.id,
-      gross_amount: (charge.amount / 100.0),
-      gross_amount_currency_id: charge.currency,
-      fee_amount: localize_charge_fee(charge.fee/100.0, backer.project.currency),
-      card_number: charge.payment_method.last4,
-      card_brand: charge.payment_method.brand,
-      card_name: charge.payment_method.name,
-      card_issuer: charge.payment_method.issuer,
-      expiration_month: charge.payment_method.exp_month,
-      expiration_year: charge.payment_method.exp_year
+      user: current_user,
+      project: project,
+      country: @visitor_country.alpha2,
+      value: calculate_backer_value.round(2),
+      currency_value: params[:backing_amount],
+      currency_code: @current_currency.iso_code,
+      payment_method: 'Conekta',
+      payment_token: params[:conektaTokenId]
     }
   end
 
-  def failed_order_post_process
-    backer.update_attributes transaction_id: order.id, failure_code: order.failure_code
-    result[:status] = "declined"
-    FondeadoraMailer.failed_card_back(backer).deliver_later!
+  def calculate_backer_value
+    params[:backing_amount].to_f * Rails.cache.read(project.currency.downcase).to_f / @current_rate
   end
 
-  # CONEKTA METHODS -> Extract once finished.
+  def verify_recaptcha?
+    Rails.env.production? && params["g-recaptcha-response"]
+  end
+  
+  def reward
+    @reward ||= Reward.find(params[:reward_id])
+  rescue ActiveRecord::RecordNotFound
+    @error = 'Reward missing.'
+    nil
+  end
+
+  # START CONEKTA METHODS -> more to wrapper.
+
+  def customer
+    @customer ||= create_customer
+  rescue StandardError
+    nil
+  end
+
+  def create_customer
+    return nil if params[:conektaTokenId].nil?
+    Conekta::Customer.create(customer_params)
+  end
+
+  def customer_params
+    {
+      name: current_user.name,
+      email: current_user.email,
+      payment_sources: payment_sources
+    }
+  end
+
+  def payment_sources
+    [{
+      type: 'card',
+      token_id: params[:conektaTokenId]
+    }]
+  end
 
   def order
     @order ||= create_order
@@ -95,100 +150,45 @@ class ConektaController < ApplicationController
     }]
   end
 
-  def customer
-    @customer ||= create_customer
-  rescue StandardError
-    nil
-  end
-
-  def create_customer
-    return nil if params[:conektaTokenId].nil?
-    Conekta::Customer.create(customer_params)
-  end
-
-  def customer_params
-    {
-      name: current_user.name,
-      email: current_user.email,
-      payment_sources: payment_sources
-    }
-  end
-
-  def payment_sources
-    [{
-      type: 'card',
-      token_id: params[:conektaTokenId]
-    }]
-  end
-
   # END CONEKTA METHODS
-
-  def preparations_failed?
-    project.nil? ||
-    backer.nil? ||
-    backer.errors.any? ||
-    customer.nil? ||
-    order.nil?
-  end
-
-  def render_json_error
-    result[:status] ||= 'error'
-    result[:message] =  @error || 'Something went wrong :('
-    render json: result.to_json
-  end
-
-  def project
-    @project ||= Project.friendly.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    @error = 'Project missing.'
-    nil
-  end
-
-  def reward
-    @reward ||= Reward.find(params[:reward_id])
-  rescue ActiveRecord::RecordNotFound
-    @error = 'Reward missing.'
-    nil
-  end
 
   def result
     @result ||= Hash.new
   end
 
-  def backer
-    @backer ||= create_backer
-  rescue StandardError
-    @error = backer.errors.full_messages.join(', ') if backer.errors.any?
-    @error ||= 'Backer missing.'
-    nil
+  def order_post_process(order)
+    return paid_order_post_process if order.payment_status == "paid"
+    failed_order_post_process
   end
 
-  def create_backer
-    Backer.create(backer_params) do |backer|
-      verify_recaptcha(model: backer, message: "Failed recaptcha.") if verify_recaptcha?
-      backer.reward = reward if reward.present? && backer.value >= reward.minimum_value 
-    end
+  def paid_order_post_process
+    backer.update_attributes(back_success_attributes)
+    backer.confirm!
+    send_successful_back_emails(backer)
+    result[:status] = "approved"
+    result[:redirect] = success_conektum_path(backer)
   end
 
-  def verify_recaptcha?
-    Rails.env.production? && params["g-recaptcha-response"]
-  end
-
-  def backer_params
+  def back_success_attributes
+    charge = order.charges.first
     {
-      user: current_user,
-      project: project,
-      country: @visitor_country.alpha2,
-      value: calculate_backer_value.round(2),
-      currency_value: params[:backing_amount],
-      currency_code: @current_currency.iso_code,
-      payment_method: 'Conekta',
-      payment_token: params[:conektaTokenId]
+      transaction_id: order.id,
+      gross_amount: (charge.amount / 100.0),
+      gross_amount_currency_id: charge.currency,
+      fee_amount: localize_charge_fee(charge.fee/100.0, backer.project.currency),
+      card_number: charge.payment_method.last4,
+      card_brand: charge.payment_method.brand,
+      card_name: charge.payment_method.name,
+      card_issuer: charge.payment_method.issuer,
+      expiration_month: charge.payment_method.exp_month,
+      expiration_year: charge.payment_method.exp_year
     }
   end
 
-  def calculate_backer_value
-    params[:backing_amount].to_f * Rails.cache.read(project.currency.downcase).to_f / @current_rate
+  def failed_order_post_process
+    backer.update_attributes transaction_id: order.id, failure_code: order.failure_code
+    result[:status] = "declined"
+    FondeadoraMailer.failed_card_back(backer).deliver_later!
   end
 
   def load_current_rate
